bd4ea29bbc22d2930f53dec297c9d565881a437a
dimas <kabanooovd@yandex.ru>
2022-07-13 22:08:49 +0300

    fixed port for deploy

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const props = process.env.PORT || 5000
+
 app.get("/", (req: any, res: any) => {
     res.send("check back 222")
 });
 
 app.listen(5000, () => {
-    console.log("app has startd")
+    console.log(`app has startd on ${props} app`)
 })
