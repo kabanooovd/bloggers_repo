43e7dc048e8daf7259cecdce81c532d948d0bd37
dimas <kabanooovd@yandex.ru>
2022-07-13 21:43:07 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req: any, res: any) => {
+//     res.send("sdsdsdsd")
+// });
+
+// app.listen(5000, () => {
+//     console.log("app has startd")
+// })
+
+console.log("dimas")
