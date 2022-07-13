348e4c93b65cdc6887242518e3586f36570f2f00
dimas <kabanooovd@yandex.ru>
2022-07-13 22:17:09 +0300

    fixed port 3

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const props = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.get("/", (req: any, res: any) => {
     res.send("check back 222")
 });
 
-app.listen(props, () => {
-    console.log(`app has startd on ${props} app`)
+app.listen(port, () => {
+    console.log(`app has startd on ${port} app`)
 })
