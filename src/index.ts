79526164f4ed0a58f2c4a1417a014019c9377531
dimas <kabanooovd@yandex.ru>
2022-07-13 21:47:57 +0300

    check if works

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-// app.get("/", (req: any, res: any) => {
-//     res.send("sdsdsdsd")
-// });
+app.get("/", (req: any, res: any) => {
+    res.send("check back")
+});
 
-// app.listen(5000, () => {
-//     console.log("app has startd")
-// })
-
-console.log("dimas")
+app.listen(5000, () => {
+    console.log("app has startd")
+})
