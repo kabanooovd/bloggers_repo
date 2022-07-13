537b098ee2eea34b43df3c1b7a3d2f3a202cb60f
dimas <kabanooovd@yandex.ru>
2022-07-13 22:02:28 +0300

    check 2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req: any, res: any) => {
-    res.send("check back")
+    res.send("check back 222")
 });
 
 app.listen(5000, () => {
