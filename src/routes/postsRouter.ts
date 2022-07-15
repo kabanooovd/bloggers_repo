15665680447dad7c497d54a1377d3dc25d508f8a
dimas <kabanooovd@yandex.ru>
2022-07-15 17:44:16 +0300

    created all endpoints for bloggers

--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -54,4 +54,43 @@ postsRouter.post("/", (req: Request, res: Response) => {
   }
 });
 
+postsRouter.put("/:id", (req: Request, res: Response) => {
+  const { title, shortDescription, content, bloggerId } = req.body;
+  const { id } = req.params;
+
+  const foundPost = posts.find((item) => item.id === Number(id));
+  if (!foundPost) res.status(404).send("Not Found");
+
+  if (!title || title.length > 30)
+    errorHandler(res, 400, "some message...", "title");
+
+  if (!shortDescription || shortDescription.length > 100)
+    errorHandler(res, 400, "some message...", "shortDescription");
+
+  if (!content || content.length > 1000)
+    errorHandler(res, 400, "some message...", "content");
+
+  if (!bloggerId || typeof bloggerId !== "number")
+    errorHandler(res, 400, "some message...", "bloggerId");
+
+  if (foundPost) {
+    foundPost.title = title;
+    foundPost.shortDescription = shortDescription;
+    foundPost.content = content;
+    foundPost.bloggerId = bloggerId;
+    res.status(204).send(foundPost);
+  }
+});
+
+postsRouter.delete("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const foundPost = posts.find((item) => item.id === Number(id));
+  if (!foundPost) res.status(404).send("Not Found");
+  if (foundPost) {
+    const currentIndex = posts.indexOf(foundPost);
+    posts.splice(currentIndex, currentIndex + 1);
+    res.status(204).send(posts);
+  }
+})
+
 export default postsRouter;
